780746674605fa5ab2b419b9bc81cdd96964b3ba
Philip Laureano <philip.laureano@gmail.com>
2021-02-20 17:38:53 +1100

    Added the state machine tests

---
+++ S4M/S4M.Tests/StateMachineTests.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace S4M.Tests
+{
+    public class StateMachineTests
+    {
+        [Fact(DisplayName = "The state machine should allow its derived classes to stash messages")]
+        public async Task ShouldBeAbleToStashMessages()
+        {
+            throw new NotImplementedException("TODO: Implement the ShouldBeAbleToStashMessages test");
+        }
+
+        [Fact(DisplayName = "The state machine should allow its derived classes to unstash all of the stashed messages")]
+        public async Task ShouldBeAbleToUnstashAllStashedMessages()
+        {
+            throw new NotImplementedException("TODO: Implement the ShouldBeAbleToUnstashAllStashedMessages test");
+        }
+
+        [Fact(DisplayName = "The state machine should allow its derived classes to change state")]
+        public async Task ShouldAllowDerivedClassesToChangeState()
+        {
+            throw new NotImplementedException("TODO: Implement the ShouldAllowDerivedClassesToChangeState test");
+        }
+    }
+}
